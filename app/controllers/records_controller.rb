1be5b088ac38ee719796ff3d5659f98e5decfa60
Bryan Flanagan <bflanagan78@yahoo.com>
2022-12-03 14:02:32 -0700

    Add user story 4

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,4 +2,7 @@ class RecordsController < ApplicationController
   def index
     @records = Record.all
   end
+  def show
+    @record = Record.find(params[:id])
+  end
 end
--- spec/features/records/show_spec.rb
+++ spec/features/records/show_spec.rb
@@ -1,1 +1,25 @@
 require 'rails_helper'
+#user story 4
+RSpec.describe 'Record show page' do
+  before :each do
+    @band_1 = Band.create!(name: "Tragedy", year_formed: 2000, active: false)
+    @band_2 = Band.create!(name: "Nine Shocks Terror", year_formed: 1996, active: false)
+    @band_3 = Band.create!(name: "Pink Turns Blue", year_formed: 1985, active: true)   
+    @record_1 = @band_1.records.create!(title: "S/T", format_size: 12, release_year: 2000, color_vinyl: true)
+    @record_2 = @band_1.records.create!(title: "Can We Call This Life?", format_size: 7, release_year: 2001, color_vinyl: false)
+    @record_3 = @band_1.records.create!(title: "Vengeance", format_size: 12, release_year: 2000, color_vinyl: false)
+    @record_4 = @band_2.records.create!(title: "Mobile Terror Unit", format_size: 7, release_year: 1996, color_vinyl: false)
+    @record_5 = @band_2.records.create!(title: "Zen and the Art...", format_size: 12, release_year: 1996, color_vinyl: true)
+    @record_6 = @band_3.records.create!(title: "If Two Worlds Kiss", format_size: 12, release_year: 1987, color_vinyl: true)
+    @record_7 = @band_3.records.create!(title: "Meta", format_size: 12, release_year: 1988, color_vinyl: false)
+  end
+  describe "When I visit /records/:id "
+  it 'shows the name of the record with that id and their attributes' do
+    visit "/records/#{@record_1.id}"
+    
+    expect(page).to have_content(@record_1.title)
+    expect(page).to_not have_content(@record_2.title)
+    expect(page).to have_content(@record_1.color_vinyl)
+    expect(page).to have_content(@record_1.release_year)
+  end
+end
