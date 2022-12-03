a403fcc4abfa67fbb64081e02aaab4dd59150184
Bryan Flanagan <bflanagan78@yahoo.com>
2022-12-03 11:32:13 -0700

    Add user story 1, tests pass

--- spec/features/bands/records/index_spec.rb
+++ spec/features/bands/records/index_spec.rb
@@ -1,1 +1,21 @@
 require 'rails_helper'
+#user story 1
+# For each parent table
+# As a visitor
+# When I visit '/parents'
+# Then I see the name of each parent record in the system
+RSpec.describe 'Parent index page' do
+  before :each do
+    @band_1 = Band.create!(name: "Tragedy", year_formed: 2000, active: false)
+    @band_2 = Band.create!(name: "Nine Shocks Terror", year_formed: 1996, active: false)
+    @band_3 = Band.create!(name: "Pink Turns Blue", year_formed: 1985, active: true)
+  end
+  it 'shows the name of each band when I visit /bands ' do
+    visit "/bands"
+    
+    expect(page).to have_content(@band_1.name)
+    expect(page).to have_content(@band_2.name)
+    expect(page).to have_content(@band_3.name)
+
+  end
+end
