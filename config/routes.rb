e23bec9d72ead1cfea3b5a754cbb3a2cbb07434f
Bryan Flanagan <bflanagan78@yahoo.com>
2022-12-03 17:30:04 -0700

    Add user story 8 tests pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
- get '/bands', to: 'bands#index'
- get '/bands/:id', to: 'bands#show'
- get '/records', to: 'records#index'
- get '/records/:id', to: 'records#show'
- get '/bands/:band_id/records', to: 'band_records#index'
+  get '/bands', to: 'bands#index'
+  get '/bands/:id', to: 'bands#show'
+  get '/records', to: 'records#index'
+  get '/records/:id', to: 'records#show'
+  get '/bands/:band_id/records', to: 'band_records#index'
 end
